3801425a070879aa188cd8ae8288805863b71638
m-aluya <maluya4@gmail.com>
2023-12-04 12:00:08 +0100

    Iterators

---
+++ iterators.py
@@ -0,0 +1,12 @@
+lst = ("BMW","Toyota","Tesla","Ford","Nissan","Kia","Audi")
+# lst = ["BMW","Toyota","Tesla","Ford","Nissan","Kia","Audi"]
+
+df = iter(lst)
+print(next(df))
+print(next(df))
+print(next(df))
+print(next(df))
+print(next(df))
+print(next(df))
+print(next(df))
+
